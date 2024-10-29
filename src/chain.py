a753103420c3c048f281c480ad8e93793416a27a
Abhilash Paul <paul.abhilash@hotmail.com>
2024-10-28 23:02:59 -0600

    removes unused argument

--- src/chain.py
+++ src/chain.py
@@ -7,10 +7,10 @@ import os
 from src.model_factory import GoogleGenerativeAIFactory
 
 
-def create_chain(model_name):
+def create_chain():
     model_factory: LLMModelFactory = GoogleGenerativeAIFactory(os.getenv("GOOGLE_LLM_MODEL"))
-    output_parser = JsonOutputParser(pydantic_object=HealthRecommendations)
     model = model_factory.create_model()
+    output_parser = JsonOutputParser(pydantic_object=HealthRecommendations)
     prompt = create_prompt(template)
     return prompt | model | output_parser
 
