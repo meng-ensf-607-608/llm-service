3cab80e07ca58138f7c676fd2168ee5bdbe68ee1
Abhilash Paul <paul.abhilash@hotmail.com>
2024-10-30 18:38:47 -0600

    adds dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11
+ENV PYTHONUNBUFFERED True
+
+ENV APP_HOME /llm-service
+WORKDIR $APP_HOME
+COPY src $APP_HOME/src
+COPY Pipfile Pipfile.lock serve.py $APP_HOME/
+
+RUN pip install --upgrade pip
+RUN pip install pipenv && pipenv install --system --deploy
+
+EXPOSE 8080
+CMD ["python", "serve.py"]
--- serve.py
+++ serve.py
@@ -3,7 +3,7 @@ import os
 from src.app import app
 
 def start_server():
-    uvicorn.run(app, host=os.getenv('APP_HOST', "localhost"), port=int(os.getenv('APP_PORT', 8080)))
+    uvicorn.run(app, host=os.getenv('APP_HOST', "127.0.0.1"), port=int(os.getenv('APP_PORT', 8080)))
 
 if __name__ == "__main__":
     start_server()
